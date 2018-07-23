2ad476088d1b5c0aad22f52ec6b5971d9cd1f710
hekomec <haliskomec@gmail.com>
2018-07-24 00:46:15 +0300

    Initial commit

---
+++ src/main/java/com/avkom/hekotech/macbookpro/avkom/classes/Iletisim.java
@@ -0,0 +1,52 @@
+package com.avkom.hekotech.macbookpro.avkom.classes;
+
+
+/**
+ * Created by hekotech on 16.07.2018.
+ */
+
+public class Iletisim {
+    String isim,mail,numara,id;
+
+    public Iletisim(String isim, String mail, String numara,String id) {
+        this.isim = isim;
+        this.mail = mail;
+        this.numara = numara;
+        this.id =id;
+    }
+    public Iletisim(){
+
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getNumara() {
+        return numara;
+    }
+
+    public void setNumara(String numara) {
+        this.numara = numara;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/avkom/hekotech/macbookpro/avkom/model/LvIletisim.java
@@ -0,0 +1,98 @@
+package com.avkom.hekotech.macbookpro.avkom.model;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.avkom.hekotech.macbookpro.avkom.R;
+import com.avkom.hekotech.macbookpro.avkom.classes.Iletisim;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
+/**
+ * Created by hekotech on 16.07.2018.
+ */
+
+public class LvIletisim extends RecyclerView.Adapter<LvIletisim.MyViewHolder> {
+
+
+    FirebaseDatabase database;
+    DatabaseReference myRef ;
+    private List<Iletisim> list;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView isim,mail,numara;
+        public Button ara,sil;
+
+        public MyViewHolder(View view) {
+            super(view);
+            isim = (TextView) itemView.findViewById(R.id.c_iletisim_isim);
+
+
+            mail = itemView.findViewById(R.id.c_iletisim_mail);
+            numara = itemView.findViewById(R.id.c_iletisim_numara);
+            ara = itemView.findViewById(R.id.c_iletisim_ara);
+            sil = itemView.findViewById(R.id.c_iletisim_sil);
+
+
+        }
+    }
+
+
+    public LvIletisim(List<Iletisim> eList) {
+        this.list = eList;
+    }
+
+    @Override
+    public LvIletisim.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.c_iletisim, parent, false);
+
+        return new LvIletisim.MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(LvIletisim.MyViewHolder holder, int position) {
+        final Iletisim mylist = list.get(position);
+        holder.isim.setText(mylist.getIsim());
+
+        holder.mail.setText(mylist.getMail());
+        holder.numara.setText(mylist.getNumara());
+
+
+        holder.ara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:"+mylist.getNumara()));
+                 view.getContext().startActivity(callIntent);
+            }
+        });
+        holder.sil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                database = FirebaseDatabase.getInstance();
+                myRef = database.getReference("iletisim");
+                myRef.child(mylist.getId()).removeValue();
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
